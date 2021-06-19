a85e706495948ea33cf0c0f056d519f783e9ead5
Savkiv <ivan.savkiv.igorovych>
2021-06-19 14:44:26 +0300

    Done news

--- js/app.js
+++ js/app.js
@@ -5,3 +5,17 @@ $(document).ready(function () {
     dots: true,
   });
 });
+
+$(document).ready(function () {
+  $(".news__slider").slick({
+    accessibility: true,
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow:
+      "<button type='button' class='slick-prev pull-left'><div class='news__scroll'><div class='news__arrow'></div></div></button>",
+    nextArrow:
+      "<button type='button' class='slick-next pull-right'><div class='news__scroll'><div class='news__arrow'></div></div></button>",
+  });
+});
